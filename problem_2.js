a51176c0bf3ed73bc46ec1cd9adf2ab4ca69c5b6
Shakil <nshosain@gmail.com>
2021-03-27 13:44:59 +0600

    works fine!

--- problem_2.js
+++ problem_2.js
@@ -1,10 +1,10 @@
-let factor1 = 0, factor2 = 1, fibo = 0, sum = 0;
+let factor_1 = 0, factor_2 = 1, fibo = 0, sum = 0;
 
 while (fibo < 4000000) {
     fibo % 2 == 0 ? sum += fibo : "";
-    fibo = factor1 + factor2;
-    factor1 = factor2;
-    factor2 = fibo;
+    fibo = factor_1 + factor_2;
+    factor_1 = factor_2;
+    factor_2 = fibo;
 }
 
 console.log(sum);
